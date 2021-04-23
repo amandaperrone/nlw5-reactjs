1c54bbfc9454882ac78ab0b741f7e6f7d82318ce
amandaperrone <amanda_degrande@yahoo.com.br>
2021-04-23 20:38:57 -0300

    aula 04

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import '../styles/global.scss';
 
@@ -6,17 +6,39 @@ import { Header } from "../components/Header";
 import { Player } from "../components/Player";
 
 import styles from '../styles/app.module.scss';
+import { PlayerContext } from "../contexts/PlayerContext";
 
 // fica por volta de TODAS as páginas, porém é carregador novamente toda vez
 function MyApp({ Component, pageProps }) {
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
+
+  function play(episode) {
+    setEpisodeList([episode]);
+    setCurrentEpisodeIndex(0);
+    setIsPlaying(true);
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying);
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state);
+  }
+
   return (
-    <div className={styles.wrapper}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+      <div className={styles.wrapper}>
       <main>
         <Header />
         <Component {...pageProps} />
       </main>
       <Player />
     </div>
+    </PlayerContext.Provider>
+    
   )
 }
 
