b54f8b43b0583314094bee3de8855a71c591457d
amandaperrone <amanda_degrande@yahoo.com.br>
2021-04-24 18:18:38 -0300

    aula 05

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,39 +6,20 @@ import { Header } from "../components/Header";
 import { Player } from "../components/Player";
 
 import styles from '../styles/app.module.scss';
-import { PlayerContext } from "../contexts/PlayerContext";
+import { PlayerContextProvider } from "../contexts/PlayerContext";
 
 // fica por volta de TODAS as páginas, porém é carregador novamente toda vez
 function MyApp({ Component, pageProps }) {
-  const [episodeList, setEpisodeList] = useState([]);
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-
-  function play(episode) {
-    setEpisodeList([episode]);
-    setCurrentEpisodeIndex(0);
-    setIsPlaying(true);
-  }
-
-  function togglePlay() {
-    setIsPlaying(!isPlaying);
-  }
-
-  function setPlayingState(state: boolean) {
-    setIsPlaying(state);
-  }
-
   return (
-    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play, isPlaying, togglePlay, setPlayingState }}>
+    <PlayerContextProvider>
       <div className={styles.wrapper}>
-      <main>
-        <Header />
-        <Component {...pageProps} />
-      </main>
-      <Player />
-    </div>
-    </PlayerContext.Provider>
-    
+        <main>
+          <Header />
+          <Component {...pageProps} />
+        </main>
+        <Player />
+      </div>
+    </PlayerContextProvider>
   )
 }
 
--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -8,6 +8,9 @@ import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './episode.module.scss';
+import { usePlayer } from '../../contexts/PlayerContext';
+import React from 'react';
+import Head from 'next/head';
 
 type Episode = {
     id: string;
@@ -26,8 +29,13 @@ type EpisodeProps = {
 }
 
 export default function Episode({ episode }: EpisodeProps) {
+    const { play } = usePlayer();
+
     return (
         <div className={styles.episode}>
+            <Head>
+                <title>{episode.title} | Podcastr</title>
+            </Head>
             <div className={styles.thumbnailContainer}>
                 <Link href={'/'}>
                     <button type="button">
@@ -41,7 +49,7 @@ export default function Episode({ episode }: EpisodeProps) {
                     src={episode.thumbnail}
                     objectFit="cover"
                 />
-                <button type="button">
+                <button type="button" onClick={() => play(episode)}>
                     <img src="/play.svg" alt="Tocar episódio" />
                 </button>
             </div>
@@ -63,7 +71,7 @@ export default function Episode({ episode }: EpisodeProps) {
 }
 
 export const getStaticPaths: GetStaticPaths = async () => {
-    const { data } = await api.get('episodes', { 
+    const { data } = await api.get('episodes', {
         params: {
             _limit: 2,
             _sort: 'published_at',
@@ -71,7 +79,7 @@ export const getStaticPaths: GetStaticPaths = async () => {
         }
     })
 
-    const paths = data.map(episode =>  {
+    const paths = data.map(episode => {
         return {
             params: {
                 slug: episode.id
